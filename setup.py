3d22500d954e322a962951f9b1f4687e75056772
Teodor Kostov <kostov.teodor@gmail.com>
2017-07-09 10:52:40 +0200

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='pvmanager',
+      version='0.0.1',
+      description='A VM manager written in Python.',
+      url='http://github.com/terusus/pvmanager',
+      author='Teodor Kostov',
+      license='MIT',
+      packages=['pvmanager'],
+      zip_safe=False)
