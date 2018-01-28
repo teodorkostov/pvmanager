8e8d51da35a63e843cd8c55b2e4182b9647f1dde
Teodor Kostov <kostov.teodor@gmail.com>
2018-01-28 14:17:15 +0100

    Added docstring for the media manager.

--- pvmanager/manager/media.py
+++ pvmanager/manager/media.py
@@ -5,7 +5,9 @@ from pvmanager.manager.file import FileManager
 
 
 class MediaManager(AbstractBaseController):
+  """The Media Manager handles the creation and removal of VM installations."""
   class Meta:
+    """The Media Manager meta configuration."""
     label = "media"
     description = "Media manager that handles the different VM installations."
 
@@ -14,10 +16,12 @@ class MediaManager(AbstractBaseController):
 
   @expose(hide=True)
   def default(self):
+    """Default command handler just prints out the help information."""
     self.app.args.print_help()
 
   @expose(help="List available installation media.")
   def list(self):
+    """The `list` command prints out all of the VM installations in the chosen root path."""
     base_path = self.app.config.get('myapp', 'prefix')
     file_manager = FileManager(base_path)
     self.app.render(dict(data=file_manager.list()), "list.m")
