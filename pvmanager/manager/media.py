5329874498c25af3e2a943aba18fc0f6fe4fbe1d
Teodor Kostov <kostov.teodor@gmail.com>
2018-01-28 10:25:02 +0100

    Fixed linting in media controller.

--- pvmanager/manager/media.py
+++ pvmanager/manager/media.py
@@ -9,11 +9,10 @@ class MediaManager(CementBaseController):
     stacked_type = "nested"
     description = "Media manager."
 
-  def __init__(self, basePath = "."):
+  def __init__(self, basePath="."):
     CementBaseController.__init__(self)
-    # self.fileManager = FileManager(basePath + "/media")
-    self.fileManager = FileManager(basePath)
+    self.file_manager = FileManager(basePath)
 
-  @expose(help = "List available installation media.")
+  @expose(help="List available installation media.")
   def list(self):
-    self.app.render(dict(data=self.fileManager.list()), "list.m")
+    self.app.render(dict(data=self.file_manager.list()), "list.m")
