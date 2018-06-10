c632baa5a174f65dd2f03d5458fe66b67d530bb7
Teodor Kostov <kostov.teodor@gmail.com>
2018-06-10 14:39:18 +0200

    Fixed some naming in ConfigManager.

--- pvmanager/manager/config.py
+++ pvmanager/manager/config.py
@@ -10,10 +10,10 @@ from pvmanager.abstract_base_controller import AbstractBaseController
 
 
 class ConfigManager(AbstractBaseController):
-  """The Config Manager handles the application persisted configuration in /home/$user/.$app_name/config."""
+  """The Config Manager handles the application persisted configuration in ~/.pvmanager/config."""
   class Meta:
-    """The Media Manager meta configuration."""
-    label = "config"
+    """The Config Manager meta configuration."""
+    label = 'config'
     description = """
       Config manager handles the persisted configuration settings.
       Config file is located at ~/.pvmanager/config.
@@ -24,39 +24,39 @@ class ConfigManager(AbstractBaseController):
 
   def __init__(self):
     AbstractBaseController.__init__(self)
-    self.config_path = self.home_path / "config"
+    self.config_path = self.home_path / 'config'
 
   def _setup(self, app_obj):
     """The config controller setup."""
     super(ConfigManager, self)._setup(app_obj)
 
     if not self.config_path.exists():
-      app_obj.log.info("creating config file ({})".format(self.config_path))
+      app_obj.log.info('creating config file ({})'.format(self.config_path))
       self.config_path.touch()
 
   def _validate_extra_arguments(self, count):
     size = len(self.app.pargs.extra_arguments)
     if 0 == size or count < size:
-      self.app.log.error("expected only one property")
+      self.app.log.error('expected only one property')
       return False
     return True
 
   def _render(self, result):
-    print("  {}".format(result))
+    print('  {}'.format(result))
 
   @expose(hide=True)
   def default(self):
     """Default command handler just prints out the help information."""
     self.app.args.print_help()
 
-  @expose(help="Prints a config property")
+  @expose(help='Prints a config property')
   def get(self):
     """The `get` command prints out the desired config property."""
     if self._validate_extra_arguments(1):
       prop = self.app.pargs.extra_arguments[0]
-      self._render("{prop_name} = '{prop_value}'".format(prop_name=prop, prop_value=self.get_config(prop)))
+      self._render('{prop_name} = "{prop_value}"'.format(prop_name=prop, prop_value=self.get_config(prop)))
 
-  @expose(help="Saves a config property")
+  @expose(help='Saves a config property')
   def set(self):
     """The `set` command saves the desired config property to the config file."""
     if self._validate_extra_arguments(2):
@@ -73,4 +73,4 @@ class ConfigManager(AbstractBaseController):
 
       with open(self.config_path, 'w') as config_file:
         config.write(config_file)
-        self.app.log.info("new prefix saved")
+        self.app.log.info('new prefix saved')
