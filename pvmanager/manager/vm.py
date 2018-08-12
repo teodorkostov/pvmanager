add305bfec35cf2576f7ba371729d79e4b972777
Teodor Kostov <kostov.teodor@gmail.com>
2018-08-12 18:41:53 +0200

    Getting the audio options as a dictionary for env variables.

--- pvmanager/manager/vm.py
+++ pvmanager/manager/vm.py
@@ -117,12 +117,7 @@ class VmManager(VmBaseManager):
       self.app.log.debug('QEMU arguments: {}'.format(qemu_arguments))
 
       # prepare the audio options
-      audio_arguments = ''
-      audio_options = vm_instance['qemu']['config'].get(AUDIO_KEY)
-      if audio_options is not None:
-        for option, value in audio_options.items():
-          value = value if value is not None else 'null'
-          audio_arguments = '{} {}={}'.format(audio_arguments, option, value)
+      audio_arguments = vm_instance['qemu']['config'].get(AUDIO_KEY)
 
       self.app.log.debug('audio arguments: {}'.format(audio_arguments))
 
