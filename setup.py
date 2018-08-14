8f2e3157be30d45b69c79f8fb2078e25731511ae
Teodor Kostov <kostov.teodor@gmail.com>
2018-08-14 21:22:41 +0200

    Updated the description in setup.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='pvmanager',
       version='0.0.1',
-      description='A VM manager written in Python.',
+      description='Python VM Manager',
       url='http://github.com/terusus/pvmanager',
       author='Teodor Kostov',
       license='GPLv3',
